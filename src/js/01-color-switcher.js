b83400b649d97548140e19b9aa7fee9bb9e7c3ac
MmAaYyAa <132580599+MmAaYyAa@users.noreply.github.com>
2023-09-22 21:18:00 +0100

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+let intervalId = null;
 
+btnStartEl.addEventListener('click', onClickStartChangeColor)
+    function onClickStartChangeColor(event) {
+  btnStartEl.disabled = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+btnStopEl.addEventListener('click', onClickStopChangeColor);
+    function onClickStopChangeColor(event) {
+    btnStartEl.disabled = false;
+  clearInterval(intervalId);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
